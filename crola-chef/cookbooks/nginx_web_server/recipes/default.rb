ad7c16a98c0dba2b81531ca1cb0e4a6ffc32221f
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2024-01-11 11:21:56 -0500

    WIP: Install sops in default recipe
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- crola-chef/cookbooks/nginx_web_server/recipes/default.rb
+++ crola-chef/cookbooks/nginx_web_server/recipes/default.rb
@@ -4,28 +4,54 @@
 #
 # Copyright:: 2023, The Authors, All Rights Reserved.
 
-key = data_bag_item('key_databag', 'sops-key')
-
-package 'nginx' do
-    action :install
+bash 'install_sops' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+  curl -LO https://github.com/getsops/sops/releases/download/v3.8.1/sops-v3.8.1.linux.amd64
+  mv sops-v3.8.1.linux.amd64 /usr/local/bin/sops
+  chmod +x /usr/local/bin/sops  
+  echo "Installed sops version: $(sops --version)"
+  EOH
 end
 
-service 'nginx' do
-    action [ :enable, :start ]
+puts "KEY outside resources: #{ENV['SOPS_AGE_KEY']}"
+puts "Other environment #{ENV['other']}"
+puts "ENV: #{ENV}"
+
+bash 'decrypt_data_bags' do
+  user 'root'
+  cwd '/home/ubuntu/'
+  default_env true
+  environment 'SOPS_AGE_KEY' => ENV['SOPS_AGE_KEY']
+  code <<-EOH
+  echo "Current key: $(echo $SOPS_AGE_KEY)"
+  sops -d -i chef-nginx-ws/crola-chef/data_bags/key_databag/sops-key.json
+  EOH
 end
 
-cookbook_file "/var/www/html/index.html" do
-  source "index.html"
-  mode "0644"
-end
+#key = data_bag_item('key_databag', 'sops-key')
 
-template "/etc/nginx/nginx.conf" do   
-  source "nginx.conf.erb"
-  notifies :reload, "service[nginx]"
-end
+#package 'nginx' do
+#    action :install
+#end
 
-file '/home/key' do
-  content key['key']
-  action :create
-end
+#service 'nginx' do
+#    action [ :enable, :start ]
+#end
+
+#cookbook_file "/var/www/html/index.html" do
+#  source "index.html"
+#  mode "0644"
+#end
+
+#template "/etc/nginx/nginx.conf" do
+#  source "nginx.conf.erb"
+#  notifies :reload, "service[nginx]"
+#end
+
+#file '/home/key' do
+#  content key['key']
+#  action :create
+#end
 
